b97282963033447c6343a108500ead07fb69b262
Dmitriy <zelentsov.dmitry@gmail.com>
2022-08-15 20:59:01 +0300

    add Ex2

---
+++ Ex2/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход
+//и выдает  количесво чифр в числе
+
+void FindColl(int a)
+{
+    string len = a.ToString().Length;
+    int b = len.Length();
+    Console.WriteLine(b);
+}
+FindColl(1234583);
