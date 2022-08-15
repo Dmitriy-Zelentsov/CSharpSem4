98f09a9fbf20089d16d87bf538b808fd80223538
Dmitriy <zelentsov.dmitry@gmail.com>
2022-08-15 21:42:10 +0300

    add Ex4`# Please enter the commit message for your changes. Lines starting

---
+++ Ex4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит массив из 8 элементов
+// заполненный нулями и единицами в случайном порядке
+//[1,1,1,0,1,0,1]
+
+void FindArr()
+{
+int[] array = new int[8];
+Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+    {
+    array[i] = rand.Next(2);
+    Console.Write($"{array[i]} ");
+    }
+}
+ FindArr();
