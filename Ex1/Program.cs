e3711820769e63c419bf7feef3c1ce9822b40962
Dmitriy <zelentsov.dmitry@gmail.com>
2022-08-15 20:19:42 +0300

    add Ex1

---
+++ Ex1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, котоорая принимает на вход число А
+//и выдает сумму чисел от 1 до А
+
+int FindSumm (int a)
+{
+    int sum = 0;
+    for(int i = 0; i <= a; i++)
+    {
+        sum += i;
+    }
+    return sum;   
+}
+Console.WriteLine (FindSumm(10));
