6c71b5d01329cf7fa0fb044564555ba71ae419f4
Dmitriy <zelentsov.dmitry@gmail.com>
2022-08-15 21:17:04 +0300

    add Ex3

---
+++ Ex3/Program.cs
@@ -0,0 +1,12 @@
+﻿//Напишите тимаеторая прин которая принимает на вход число А
+// и выводит произведение этих чисед от 1 до А
+ int FindFactor(int a)
+ {
+    int resalt = 1;
+    for(int i = 1; i <= a; i++)
+    {
+        resalt *= i;
+    }
+    return resalt;
+ }
+Console.WriteLine (FindFactor(5));
